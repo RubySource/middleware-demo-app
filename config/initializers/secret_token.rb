263a06dfbaaeaa2884d535ae5655e53dafa8c79e
Dan Cheail <dan@undumb.com>
2011-05-04 20:33:02 +1000

    Empty App, rspec+capybara

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MiddlewareDemo::Application.config.secret_token = '00b25d4a2522172f51114659a26bc7b46cee258900ebf59744ea2f56f80b748282623efc06eb22aaf3afd5f0cf7487cb81982dca5973f23da77351313d292b90'
